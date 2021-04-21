86873321d5281814e8b500aa2b14cf7facf70091
Oleguer Sagarra <ula@dribia.com>
2021-04-21 20:32:00 +0200

    Added tester and env files

--- tests/test_driganttic.py
+++ tests/test_driganttic.py
@@ -6,6 +6,13 @@ Dribia 2021/01/11, Albert Iribarne <iribarne@dribia.com>
 import re
 
 import driganttic
+import os
+from dotenv import load_dotenv
+
+# API KEY is stored in the env file
+load_dotenv()
+
+APIKEY = os.getenv('APIKEY')
 
 
 def test_version():
